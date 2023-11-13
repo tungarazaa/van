94b303fc06e0a68c18eb376d98c6b0a726eac565
tungarazaa <tungarazakennedy@gmail.com>
2023-11-13 15:41:51 +0300

    Adding pages

---
+++ pages/VanDetail.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+
+export default function VanDetail(){
+    const [van,setVan] = React.useState(null)
+    const param = useParams()
+
+    React.useEffect(()=>{
+        fetch(`/api/vans/${param.id}`)
+        .then(res=>res.json())
+        .then(data=> setVan(data.vans))
+    }, [param.id])
+
+    return(
+        <>
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </>
+    )
+}
