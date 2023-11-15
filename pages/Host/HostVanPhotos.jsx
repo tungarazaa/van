a4d27763ff26a961273e7488f8862b908288ad5f
tungarazaa <tungarazakennedy@gmail.com>
2023-11-15 09:35:25 +0300

    Adding layouts

---
+++ pages/Host/HostVanPhotos.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+
+export default function HostVanPhotos(){
+    const { currentVan } = useOutletContext()
+    
+    return(
+        <img src={currentVan.imageUrl} className="host-van-detail-image" />
+    )
+}
